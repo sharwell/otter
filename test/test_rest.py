916e98b574271985dbc114849967e2033ecbcce8
cyli <ying.li@rackspace.com>
2012-11-27 19:22:07 -0500

    Code cleanup of the web service fixture - put in empty list test

--- test/test_rest.py
+++ test/test_rest.py
@@ -10,7 +10,6 @@ from urlparse import urlunsplit
 from twisted.internet import defer, endpoints
 from twisted.trial.unittest import TestCase
 from twisted.web.client import Agent, RedirectAgent
-from twisted.web.http import HTTPChannel
 from twisted.web.server import Site
 
 from otter.models.mock import MockScalingGroup
@@ -20,102 +19,106 @@ from otter.util.http import request
 from test.utils import fixture
 
 
-class DisconnectionNotifyingChannel(HTTPChannel):
+class _PausableSite(Site):
     """
-    Channel that informs the factory when the connection has been lost
+    A site that can be "paused" - a regular ``twisted.web.server.Site`` can't
+    really exist between test methods, because ``Site`` schedules updating its
+    log date time call, which leaves the reactor unclean because one is always
+    scheduled when a test method ends.
+
+    This subclass has pause and resume methods that cancel the scheduled call
+    and schedule a new call, respectively.
     """
-    def connectionLost(self, *args, **kwargs):
-        """
-        Connection lost that also tells its factory that it has disconnected.
-        """
-        HTTPChannel.connectionLost(self, *args, **kwargs)
-        print 'connectionLost'
-        # factory = getattr(self, 'factory', None)
-        # if factory:
-        #     disconnected = getattr(factory, 'disconnected', None)
-        #     if disconnected:
-        #         disconnected.callback(None)
-
-
-class DisconnectionNotifyingSite(Site):
-    """
-    Site that has a ``disconnected`` deferred that callbacks when the protocol
-    has lost its connection.
-    """
-    protocol = DisconnectionNotifyingChannel
-    disconnected = None
-
-    def buildProtocol(self, *args, **kwargs):
-        print 'buildProtocol'
-        proto = Site.buildProtocol(self, *args, **kwargs)
-        self.disconnected = defer.Deferred()
-        proto.factory = self
-        return proto
-
-    def stopFactory(self, *args, **kwargs):
-        print 'stop factory'
-        Site.stopFactory(self, *args, **kwargs)
-        self.disconnected.callback(None)
-
     def pause(self):
+        """
+        Cancel the scheduled `_updateLogDateTime()` call
+        """
         if (self._logDateTimeCall is not None and
                 self._logDateTimeCall.active()):
             self._logDateTimeCall.cancel()
             self._logDateTimeCall = None
 
     def resume(self):
+        """
+        Make a call to `_updateLogDateTime()` (which also schedules the next
+        such call)
+        """
         self._updateLogDateTime()
 
 
-class RestService(object):
+class WebServiceTestFixture(object):
     """
-    Test resource/fixture builder that can build a REST web service
+    A twisted web service test fixture that can run the web service when
+    needed, pauses the web service when `cleanupService()` is called, and
+    resumes the web service when `buildService()` is called.
     """
-    listening_port_deferred = None
-    listening_port = None
-    site = None
-    port = 8080
+    services = {}
 
     @classmethod
-    def start(cls):
-        if cls.site is None:
+    def buildService(cls, port, resource):
+        """
+        Creates a web service with the given resource that runs on the given
+        port, if no service is running on the port already.  This service can
+        be paused by calling `cleanupService()`, but once built, will keep
+        running until the reactor shuts down.
+
+        If the web service is already running, resumes it if it is paused.
+
+        Will raise an exception if a different resource's web service is
+        already running on the same port.
+
+        :param port: the port to run the web service on
+        :type port: ``int``
+
+        :param resource: the resource to use as the root of the site
+        :type resource: ``twisted.web.resource.Resource``
+        """
+        if port not in cls.services:
+            cls.services[port] = {
+                'resource': resource,
+                'site': _PausableSite(resource)
+            }
+            service = cls.services[port]  # shorthand
+
             from twisted.internet import reactor
-            cls.site = DisconnectionNotifyingSite(root)
-            endpt = endpoints.serverFromString(reactor,
-                                               'tcp:{0}'.format(cls.port))
-            cls.listening_port_deferred = endpt.listen(cls.site)
-
-            cls.disconnected = defer.Deferred()
-
-            def stop_it():
-                return defer.gatherResults([
-                    defer.maybeDeferred(cls.listening_port.stopListening),
-                    cls.disconnected,
-                    cls.site.disconnected
-                ])
+            endpt = endpoints.serverFromString(reactor, 'tcp:{0}'.format(port))
 
             def on_ready(listening_port):
-                cls.listening_port = listening_port
-                on_connection_lost = listening_port.connectionLost
+                service['listening'] = listening_port
+                reactor.addSystemEventTrigger("before", "shutdown",
+                                              listening_port.stopListening)
 
-                def new_connection_lost(*args, **kwargs):
-                    print 'connection really lost'
-                    on_connection_lost(*args, **kwargs)
-                    cls.disconnected.callback(None)
+            # endpoint.listen() returns a deferred that callbacks with a
+            # listening port
+            return endpt.listen(service['site']).addCallback(on_ready)
 
-                listening_port.connectionLost = new_connection_lost
-                reactor.addSystemEventTrigger("before", "shutdown", stop_it)
+        service = cls.services[port]
 
-            return cls.listening_port_deferred.addCallback(on_ready)
-        elif cls.listening_port is not None:
-            cls.site.resume()
-            return defer.maybeDeferred(cls.listening_port.resumeProducing)
+        if service['resource'] is not resource:
+            raise Exception(
+                '{curr!r} is already running on {port} - cannot run {want!r}'
+                .format(curr=service['resource'], port=port, want=resource))
+
+        if 'listening' in service:
+            service['site'].resume()
+            # port.resumeProducing puts the listening port back in the reactor
+            return defer.maybeDeferred(service['listening'].resumeProducing)
 
     @classmethod
-    def stop(cls):
-        if cls.site is not None and cls.listening_port is not None:
-            cls.site.pause()
-            return defer.maybeDeferred(cls.listening_port.pauseProducing)
+    def cleanupService(cls, port):
+        """
+        Pauses an already existing web service on the given port.
+
+        :param port: the port to run the web service on
+        :type port: ``int``
+        """
+        if port in cls.services:
+            service = cls.services[port]
+            service['site'].pause()
+            if 'listening' in service:
+                # port.pauseProducing removes the listening port from the
+                # reactor
+                return defer.maybeDeferred(service['listening'].pauseProducing)
 
 
 class TestRestAbstractBaseMixin(object):
@@ -142,7 +145,7 @@ class TestRestAbstractBaseMixin(object):
     """
     fixture = "test_rest_fixture.json"
     scheme = 'http'
-    port = RestService.port
+    port = 8080
 
     _agent = None
 
@@ -168,16 +171,15 @@ class TestRestAbstractBaseMixin(object):
         return urlunsplit(
             (self.scheme, 'localhost:{0}'.format(self.port), path, None, None))
 
-    # @defer.inlineCallbacks
-    # def test_empty_get_all_scaling_groups(self):
-    #     """
-    #     Get list of scaling groups of a tenant ID that has no scaling groups
-    #     """
-    #     wrapper = yield request(self.agent, 'GET', self.get_url('/v1.0/11111'))
-    #     self.assertEqual(200, wrapper.response.code)
-    #     self.assertEqual(json.loads(wrapper.content), {})
-
-    # test_empty_get_all_scaling_groups.skip = "This is broken now"
+    @defer.inlineCallbacks
+    def test_empty_get_all_scaling_groups(self):
+        """
+        Get list of scaling groups of a tenant ID that has no scaling groups
+        """
+        wrapper = yield request(self.agent, 'GET',
+                                self.get_url('/v1.0/11111/scaling_groups'))
+        self.assertEqual(200, wrapper.response.code)
+        self.assertEqual(json.loads(wrapper.content), {})
 
     @defer.inlineCallbacks
     def test_create_scaling_group(self):
@@ -207,13 +209,15 @@ class TestRestAbstractBaseMixin(object):
         self.assertEqual(1, len(headers))
 
         # now make sure the Location header points to something good!
-        # try:
-        #     wrapper = yield request(self.agent, 'GET', headers[0])
-        # except:
-        #     self.fail("Unable to connect to the URI in the location header")
-        # else:
-        #     self.assertEqual(wrapper.response.code, 200)
-        #     self.assertEqual(json.load(wrapper.content), config)
+        try:
+            wrapper = yield request(self.agent, 'GET', headers[0])
+        except:
+            self.fail("Unable to connect to the URI in the location header")
+        else:
+            self.assertEqual(wrapper.response.code, 200)
+            self.assertEqual(json.load(wrapper.content), config)
+
+    test_create_scaling_group.skip = "Location header currently broken"
 
 
 class MockStoreRestTestCase(TestRestAbstractBaseMixin, TestCase):
@@ -240,13 +244,10 @@ class MockStoreRestTestCase(TestRestAbstractBaseMixin, TestCase):
         get_store().data = data
 
         # make sure the rest service is started
-        return RestService.start()
-
-    def test_pass(self):
-        pass
+        return WebServiceTestFixture.buildService(self.port, root)
 
     def tearDown(self):
         """
         Remove the rest server's listening port from the reactor
         """
-        return RestService.stop()
+        return WebServiceTestFixture.cleanupService(self.port)
